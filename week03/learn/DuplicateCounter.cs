a72e537006d8bbdf5ce3b809e58f07a14d11dd08
Declan Ruediger <declan.ruediger@gmail.com>
2026-03-18 16:10:20 +1000

    W3 - Learning Activity

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -1,4 +1,6 @@
-﻿public class DuplicateCounter
+﻿using System.Dynamic;
+
+public class DuplicateCounter
 {
     //Count how many duplicates are in a collection of data.
 
@@ -25,6 +27,8 @@
     private static int CountDuplicates(int[] data)
     {
         // Add code here.
-        return 0;
+        var deduplicatedData = data.ToHashSet();
+
+        return data.Length - deduplicatedData.Count;
     }
 }
